88cebc5f44332a65a391af36d8540b096b315ff2
Andrew Kim <ndbkim@gmail.com>
2026-01-30 19:24:34 -0800

    more uauth

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
+  # root 'sessions#home'
+
+  resource :users, only: [:new, :create, :edit, :update, :show, :destroy]
+
   resource :session
+  resource :sign_up
+
   resources :passwords, param: :token
-  resources :create_cat do
-    collection do
-      get :staff_credentials
-    end
-  end
+  resources :create_cat
+  
   root 'create_cat#index'
 end
