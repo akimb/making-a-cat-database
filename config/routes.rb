942a56b22a63bba586276c83e3f6fb9adef8a25a
Andrew Kim <ndbkim@gmail.com>
2026-02-10 10:43:16 -0800

    favicon and proper routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,16 @@
 Rails.application.routes.draw do
   # root 'sessions#home'
 
-  resource :users, only: [:new, :create, :edit, :update, :show, :destroy]
+  ## USER RESOURCE IS LEFT UP HERE AS A REMINDER. NOW, SIGNUPSCONTROLLER IS HANDLING USERS.
+  # resource :users, only: [:new, :create, :edit, :update, :show, :destroy]
 
+  # resource (singular) for things that won't need any more than 1 thing at a time, like a user's session
   resource :session
   resource :sign_up
   resource :about_us, controller: 'about_us'
   resource :career_page
 
+  # resources (plural) for things that need multiple instances, i.e. the cats 
   resources :passwords, param: :token
   resources :create_cat
   
